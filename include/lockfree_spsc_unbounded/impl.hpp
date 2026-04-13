4bf33a8c0ad2891bf93897659e7455cc3491e439
Sujal-Patnaik <sujalpatnaik5@gmail.com>
2026-04-13 21:36:20 +0530

    added push and try_pop in spsc unbounded

--- include/lockfree_spsc_unbounded/impl.hpp
+++ include/lockfree_spsc_unbounded/impl.hpp
@@ -7,11 +7,23 @@ template <typename T>
 using queue = tsfqueue::impl::lockfree_spsc_unbounded<T>;
 
 // template <typename T> void queue<T>::push(T value) {
-    
+    node* temp = tail.load(std::memory_order_relaxed);
+    temp->data = std::move(value);
+    node* new_tail = new node();
+    new_tail->next.store(nullptr,std::memory_order_relaxed);
+    temp->next.store(new_tail,std::memory_order_release);
+    tail.store(new_tail,std::memory_order_relaxed);
 // }
 // head -> stub <-tail
 
 // template <typename T> bool queue<T>::try_pop(T &value) {
+    node* old=head;
+    node* nxt = old->next.load(std::memory_order_acquire);
+    if(nxt==nullptr) return false;
+    value = std::move(old->data);
+    head = nxt;
+    delete old;
+    return true;
 // }
 
 // template <typename T> void queue<T>::wait_and_pop(T &value) {
