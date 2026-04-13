1411d54397c03185a713ee0535287455f980f613
Sujal-Patnaik <sujalpatnaik5@gmail.com>
2026-04-13 22:50:23 +0530

    Optimize atomic load operations in lockfree SPSC queue
    Changed atomic load operations from acquire to relaxed for tail and head variables to improve performance, as only the producer thread modifies tail and consumer thread modifies head

--- include/lockfree_spsc_bounded/impl.hpp
+++ include/lockfree_spsc_bounded/impl.hpp
@@ -7,7 +7,7 @@
 namespace tsfqueue::impl {
 template <typename T, size_t Capacity>
 void lockfree_spsc_bounded<T, Capacity>::wait_and_push(T value) {
-  size_t cur_tail = tail.load(std::memory_order_acquire);
+  size_t cur_tail = tail.load(std::memory_order_relaxed);
   //We should change the tail.load(acquire) to tail.load(relaxed) since only the producer thread has 
   // exclusive write access to the tail variable and a single thread always agrees upon the order of modification of an atomic variable.
   //tail.load(relaxed) is less costly.
@@ -30,7 +30,10 @@ bool lockfree_spsc_bounded<T, Capacity>::try_push(T value) {
 template <typename T, size_t Capacity>
 bool lockfree_spsc_bounded<T, Capacity>::try_pop(T &value) {
   // cur_tail = tail.load(std::memory_order_acquire);
-  size_t cur_head = head.load(std::memory_order_acquire);
+  size_t cur_head = head.load(std::memory_order_relaxed);
+  //We should change the head.load(acquire) to head.load(relaxed) since only the producer thread has 
+  // exclusive write access to the head variable and a single thread always agrees upon the order of modification of an atomic variable.
+  //head.load(relaxed) is less costly.
   if (tail_cache == cur_head) {
     tail_cache = tail.load(std::memory_order_acquire);
     if (tail_cache == cur_head)
@@ -45,7 +48,10 @@ bool lockfree_spsc_bounded<T, Capacity>::try_pop(T &value) {
 
 template <typename T, size_t Capacity>
 void lockfree_spsc_bounded<T, Capacity>::wait_and_pop(T &value) {
-  size_t cur_head = head.load(std::memory_order_acquire);
+  size_t cur_head = head.load(std::memory_order_relaxed);
+  //We should change the head.load(acquire) to head.load(relaxed) since only the producer thread has 
+  // exclusive write access to the head variable and a single thread always agrees upon the order of modification of an atomic variable.
+  //head.load(relaxed) is less costly.
   while (tail_cache == cur_head) {
     tail_cache = tail.load(std::memory_order_acquire); // busy wait
   }
@@ -71,7 +77,10 @@ bool lockfree_spsc_bounded<T, Capacity>::peek(T &value) {
 template <typename T, size_t Capacity>
 template <typename... Args>
 bool lockfree_spsc_bounded<T, Capacity>::emplace_back(Args &&...args) {
-  size_t cur_tail = tail.load(std::memory_order_acquire);
+  size_t cur_tail = tail.load(std::memory_order_relaxed);
+  //We should change the tail.load(acquire) to tail.load(relaxed) since only the producer thread has 
+  // exclusive write access to the tail variable and a single thread always agrees upon the order of modification of an atomic variable.
+  //tail.load(relaxed) is less costly.
   size_t next_tail = (cur_tail + 1) % capacity;
   if (next_tail == head_cache) {
     head_cache = head.load(std::memory_order_acquire);
