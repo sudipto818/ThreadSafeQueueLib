0e2121e6d821693b39f823f67b5c0c00f8edd242
Sujal Patnaik <sujalpatnaik5@gmail.com>
2026-05-31 22:35:59 +0530

    lockfree_mpsc_unbounded

---
+++ include/lockfree_mpsc_unbounded/defs.hpp
@@ -0,0 +1,65 @@
+#ifndef LOCKFREE_MPSC_UNBOUNDED_DEFS
+#define LOCKFREE_MPSC_UNBOUNDED_DEFS
+
+#include "utils.hpp"
+#include <atomic>
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace tsfqueue::impl {
+template <typename T, typename Allocator = std::allocator<T>> class lockfree_spsc_unbounded {
+	static_assert(std::is_object_v<T>, "T must be an object type");
+	static_assert(!std::is_reference_v<T>, "Queue cannot store reference types");
+	static_assert(std::is_default_constructible_v<T>, "T must be default constructible");
+	static_assert(std::is_move_constructible_v<T> || std::is_copy_constructible_v<T>,
+				  "T must be move or copy constructible");
+	static_assert(std::is_move_assignable_v<T> || std::is_copy_assignable_v<T>,
+				  "T must be move or copy assignable");
+	static_assert(std::is_nothrow_destructible_v<T>, "T must be nothrow destructible");
+
+	using node = tsfqueue::utils::Lockless_Node<T>;
+
+	using node_allocator = typename std::allocator_traits<Allocator>::template rebind_alloc<node>;
+	using node_alloc_traits = std::allocator_traits<node_allocator>;
+
+    public:
+      lockfree_mpsc_unbounded();
+
+      lockfree_mpsc_unbounded(const lockfree_mpsc_unbounded &) = delete;
+      lockfree_mpsc_unbounded &operator=(const lockfree_mpsc_unbounded &)=delete;
+
+      lockfree_mpsc_unbounded(lockfree_mpsc_unbounded &&other) noexcept;
+
+      lockfree_mpsc_unbounded &operator=(lockfree_mpsc_unbounded &&other) noexcept;
+
+      ~lockfree_mpsc_unbounded();
+
+      void swap(lockfree_mpsc_unbounded &other) noexcept;
+
+      void push(T value);
+
+      template<typename... Args>
+      void emplace(Args &&...args);
+
+      void wait_and_pop(T& value);
+      bool try_pop(T& value);
+
+      [[nodiscard]] bool empty() const;
+      [[nodiscard]] bool peek(T& value) const;
+      [[nodiscard]] size_t size() const noexcept;
+
+    private:
+      node* allocate_node();
+      void deallocate_node(node* p) noexcept;
+
+      node_allocator alloc{};
+
+      alignas(cache_line_size) node* head{};
+      alignas(cahce_line_size) std::atomic<node*> tail{};
+      alignas(cache_line_size) std::atomic<size_t> size{0};
+    };
+}
+
+#endif
---
+++ include/lockfree_mpsc_unbounded/impl.hpp
@@ -0,0 +1,134 @@
+#ifndef LOCKFREE_MPSC_UNBOUNDED_IMPL
+#define LOCKFREE_MPSC_UNBOUNDED_IMPL
+
+#include<defs.hpp>
+#include<thread>
+#include<utility>
+
+namespace tsfqueue::impl {
+    template<typename T, typename Allocator>
+    lockfree_mpsc_unbounded<T,Allocator>::lockfree_mpsc_unbounded(){
+        node* stub = allocate_node();
+        head=stub;
+        tail.store(stub, std::memory_order_relaxed);
+        // memory_order is relaxed as this is the constructor 
+        // and called only once when the queue is made before the producer and consumer
+        // threads start accessing the queue.
+    }
+    template<typename T, typename Allocator>
+    lockfree_mpsc_unbounded<T,Allocator>::lockfree_mpsc_unbounded(
+        lockfree_mpsc_unbounded &&other) noexcept
+        : lockfree_mpsc_unbounded(){
+            swap(other);
+        }
+    template <typename T, typename Allocator>
+    lockfree_mpsc_unbounded<T, Allocator> &
+    lockfree_mpsc_unbounded<T, Allocator>::operator=(
+	lockfree_mpsc_unbounded &&other) noexcept {
+	if (this == &other) {
+		return *this;
+	}
+
+	lockfree_mpsc_unbounded tmp(std::move(other));
+	swap(tmp);
+	return *this;
+    }
+
+    template<typename T, typename Allocator>
+    lockfree_mpsc_unbounded<T,Allocator>::~lockfree_mpsc_unbounded(){
+        node* curr=head;
+        while(curr!=nullptr){
+            node* nxt = curr->next.load(std::memory_order_relaxed);
+            deallocate_node(curr);
+            curr=nxt;
+        }
+        //memory_order is relaxed as this is destructor and only accessed
+        //when no thread is accessing the queue so no chance of data race.
+    }
+
+    template<typename T, typename Allocator>
+    lockfree_mpsc_unbounded<T,Allocator>::swap(
+        lockfree_mpsc_unbounded &other) noexcept {
+            using std::swap;
+            swap(alloc,other.alloc);
+            swap(head,other.head);
+            node* this_tail=tail.load(std::memory_order_relaxed);
+            node* other_tail=other.tail.load(std::memory_order_relaxed);
+            tail.store(other_tail,std::memory_order_relaxed);
+            other.tail.store(this_tail,std::memory_order_relaxed);
+        }
+    
+    template <typename T, typename Allocator>
+    typename lockfree_spsc_unbounded<T, Allocator>::node *
+    lockfree_spsc_unbounded<T, Allocator>::allocate_node() {
+	node *p = node_alloc_traits::allocate(alloc, 1);
+	node_alloc_traits::construct(alloc, p);
+	p->next.store(nullptr, std::memory_order_relaxed);
+	return p;
+   }
+
+   template <typename T, typename Allocator>
+   void lockfree_mpsc_unbounded<T, Allocator>::deallocate_node_(node *p) noexcept {
+	node_alloc_traits::destroy(alloc, p);
+	node_alloc_traits::deallocate(alloc, p, 1);
+  }
+
+  template <typename T, typename Allocator>
+  void lockfree_mpsc_unbounded<T, Allocator>::push(T value) {
+	emplace(std::move(value));
+}
+
+  template <typename T, typename Allocator>
+  template <typename... Args>
+  void lockfree_mpsc_unbounded<T, Allocator>::emplace(Args &&...args) {
+	node *new_stub = allocate_node();
+    node *old_tail = tail.exchange(new_stub, std::memory_order_acq_rel);
+    // The above memory ordering is to be discussed further.
+	old_tail->data = T(std::forward<Args>(args)...);
+	old_tail->next.store(new_stub, std::memory_order_release);
+	size.fetch_add(1, std::memory_order_relaxed);
+}
+  template <typename T, typename Allocator>
+  bool lockfree_mpsc_unbounded<T,Allocator>::try_pop(T& value){
+    node *old_head = head;
+    node *nxt = old_head->next.load(std::memory_order_acquire);
+    if(nxt==nullptr) return false;
+    value = std::move(old_head->data);
+    head=nxt;
+    deallocate(old_head);
+    size.fetch_add(1, std::memory_order_relaxed);
+    return true;
+  }
+
+  template <typename T, typename Allocator>
+  void lockfree_spsc_unbounded<T, Allocator>::wait_and_pop(T &value) {
+	while (!try_pop(value)) {
+		std::this_thread::yield();
+	}
+  }
+
+  template <typename T, typename Allocator>
+ bool lockfree_spsc_unbounded<T, Allocator>::peek(T &value) const { //Not atomic as a whole, so it is not a proper peek operation in general except spsc.
+	static_assert(std::is_copy_assignable_v<T>, "peek() requires T to be copy assignable");
+	node *current_head = head;
+	node *next = current_head->next.load(std::memory_order_acquire);
+	if (next == nullptr) {
+		return false;
+	}
+	value = current_head->data;
+	return true;
+}
+
+ template <typename T, typename Allocator>
+ bool lockfree_spsc_unbounded<T, Allocator>::empty() const {
+	return head->next.load(std::memory_order_acquire) == nullptr;
+}
+
+ template <typename T, typename Allocator>
+ size_t lockfree_spsc_unbounded<T, Allocator>::size() const noexcept {
+	return size.load(std::memory_order_relaxed);
+}
+
+}
+
+#endif
